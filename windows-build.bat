7a1e8aa71dbdd97147f5a3c25b6aeeff5a653989
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2024-04-01 17:12:50 +0800

    ci: add builder to CI

--- windows-build.bat
+++ windows-build.bat
@@ -1,5 +1,13 @@
 git submodule update --init --recursive
 
+rem "start building the MFC C++ components........"
+cd smtp-xoauth2
+set MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Enterprise\Msbuild\Current\Bin\MSBuild.exe"
+set SOLUTION_FILE=smtp-xoauth2.sln
+set BUILD_CONFIGURATION=Release
+%MSBUILD_PATH% %SOLUTION_FILE% /t:Build /p:Configuration=%BUILD_CONFIGURATION%
+cd ..
+
 rem "start building the rust components........"
 cd remote-call
 rmdir /s /q target
@@ -14,14 +22,6 @@ cd ..
 cd emailer-service
 rmdir /s /q target
 cargo build --release
-cd ..
-
-rem "start building the MFC C++ components........"
-cd smtp-xoauth2
-set MSBUILD_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MSBuild.exe"
-set SOLUTION_FILE=smtp-xoauth2.sln
-set BUILD_CONFIGURATION=Release
-%MSBUILD_PATH% %SOLUTION_FILE% /t:Build /p:Configuration=%BUILD_CONFIGURATION%
 cd ..
 
 mkdir release
